87f9ee9e47395a81b204a0c9e22773ba7641130d
surya Vivek Janjam <suryavivek33377@gmail.com>
2022-05-01 17:39:06 +0530

    added blog link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Click <a href="https://blog.suryavivek.tech">here</a> to read the blog</p>
