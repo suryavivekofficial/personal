7a284c496798bb6b4cfef482db961128fbe3ed55
surya Vivek Janjam <suryavivek33377@gmail.com>
2022-05-01 18:10:30 +0530

    changed from template to my name

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<p>Click <a href="https://blog.suryavivek.tech">here</a> to read the blog</p>
+<h1>Hi, I'm Surya Vivek</h1>
+<p>Click <a href="https://blog.suryavivek.tech">here</a> to visit my blog</p>
